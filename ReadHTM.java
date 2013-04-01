35b0fce65df0e00f5aaac7e777238a028fee1379
yuchaozh <yuchao.zhou@intel.com>
2013-04-01 22:58:02 +0800

    read htm and extract file name but not build index

--- ReadHTM.java
+++ ReadHTM.java
@@ -8,6 +8,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * Read all htm files to find out the hyperlinks and offset
@@ -21,33 +23,54 @@ public class ReadHTM
 	InputStreamReader inputreader;
 	BufferedReader br;
 	int offset;
+	private int index;
+	private int backindex;
+	private ArrayList<Path> htmref;
+	private Path htmrefpath;
 	
-/*	public ReadHTM()
+	public ReadHTM()
 	{
-
-	}*/
+		htmref = new ArrayList<Path>();
+	}
 	
 	/**
-	 * readhtm: read the content of htm file
+	 * readhtm: read the content of all htm files
 	 * @param file: htm file
 	 */
 	public void readhtm(File file) throws IOException
 	{
+		//读取原始字节流
 		input = new FileInputStream(file);
 		inputreader = new InputStreamReader(input);
+		//用bufferedreader包装了inputstreamreader，更有效率
 		br = new BufferedReader(inputreader);
 		String info=br.readLine();
+		int line = 1;
+		int currentlength = 0;
+		int prelength = 0;
 		while(info!=null)
 		{
-			System.out.println(info);   
+			//System.out.println("line: " + line);
+			//System.out.println(info);   
+			//System.out.println("sunm of one line: " + info.length());
+			index = info.indexOf(">file:///");
+			backindex = info.lastIndexOf("</a>");
+			//System.out.println("index: " + index);
+			currentlength = info.length();
+			if (index != -1)
+			{
+				//System.out.println(info.substring(index + 9,backindex));
+				htmrefpath = Paths.get(info.substring(index + 9,backindex));
+				htmref.add(Paths.get(info.substring(index + 9,backindex)));
+				index = prelength + index;
+				//System.out.println("totle index: " + index);
+			}
 			info = br.readLine();   
+			prelength = prelength + currentlength;
+			line++;
 		}
 	}
 	
-	public void findreference()
-	{
-		
-	}
 	
 	/**
 	 * startread: use the path of htm file to create file instance to read
@@ -64,5 +87,10 @@ public class ReadHTM
 			File htmfile = new File(filepath.toString());
 			readhtm(htmfile);
 		}
+		for (int i = 0; i < htmref.size(); i++)
+		{
+			System.out.println(htmref.get(i));
+		}
+
 	}
 }
